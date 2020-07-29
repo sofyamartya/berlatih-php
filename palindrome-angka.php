085caacc18fdb4aa72a782366958b423deff94d8
Sofy Amartya <sofy_amartya@yahoo.com>
2020-07-30 00:26:15 +0700

    function palindrome finished

--- palindrome-angka.php
+++ palindrome-angka.php
@@ -7,14 +7,42 @@
     <title>Palindrome</title>
   </head>
   <body>
+    <h1>Palindrome Angka</h1>
+
     <?php
 
       function palindrome_angka($angka) {
         // tulis kode di sini
+        $result = '';
+        $reverse = '';
+        $value = strval($angka);
+        for ( $i = strlen($value) - 1; $i >= 0 ; $i-- ) {
+          $reverse .= $value[$i];
+        }
+        if ( $reverse == $value ) {
+          $angka += 1;
+        }
+
+        while (true) {
+          $value = strval($angka);
+          $reverse = '';
+          for ( $i = strlen($value) - 1; $i >= 0 ; $i-- ) {
+            $reverse .= $value[$i];
+          }
+
+          if ( $reverse == $value ) {
+            global $result;
+            $result = $value . '<br>';
+            return $result;
+          } else {
+            $angka += 1;
+          }
+        }
+
       }
 
       // TEST CASES
-      echo palindrome_angka(8) // 9
+      echo palindrome_angka(8); // 9
       echo palindrome_angka(10); // 11
       echo palindrome_angka(117); // 121
       echo palindrome_angka(175); // 181
